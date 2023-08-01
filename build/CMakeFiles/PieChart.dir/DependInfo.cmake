e5d75d71fec4e7131201607fac3c48a55c7e5d1a
Anthony Roulois <anthonyroulois506@gmail.com>
2023-08-01 13:16:14 +0200

    Création CMakeLists.txt

---
+++ build/CMakeFiles/PieChart.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/roulois/Bureau/projet/Pie-Chart/piechart.c" "CMakeFiles/PieChart.dir/Pie-Chart/piechart.c.o" "gcc" "CMakeFiles/PieChart.dir/Pie-Chart/piechart.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/PieChart.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PieChart.dir/Pie-Chart/piechart.c.o"
+  "CMakeFiles/PieChart.dir/Pie-Chart/piechart.c.o.d"
+  "PieChart"
+  "PieChart.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PieChart.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
